30432bae4bf9cff86a509c58f79065090011af37
Queenelsa1926 <87455501+Queenelsa1926@users.noreply.github.com>
2021-10-28 16:59:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.formURL('BirthdayImage.jpg'){
+        
+    }
+	
+}
+
+function playSound(){
+	x.play();
+}
